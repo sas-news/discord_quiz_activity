54839e286d15238c9b532af7e485860851180ecc
SAS NEWS <109570410+sas-news@users.noreply.github.com>
2024-08-30 14:01:03 +0900

    環境変数更新

--- client/env.d.ts
+++
@@ -1,7 +0,0 @@
-interface ImportMetaEnv {
-  readonly VITE_DISCORD_CLIENT_ID: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
---
+++ env.d.ts
@@ -0,0 +1,15 @@
+interface ImportMetaEnv {
+  readonly VITE_FIREBASE_API_KEY: string;
+  readonly VITE_FIREBASE_AUTH_DOMAIN: string;
+  readonly VITE_FIREBASE_PROJECT_ID: string;
+  readonly VITE_FIREBASE_STORAGE_BUCKET: string;
+  readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string;
+  readonly VITE_FIREBASE_APP_ID: string;
+
+  readonly VITE_DISCORD_CLIENT_ID: string;
+  readonly DISCORD_CLIENT_SECRET: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
