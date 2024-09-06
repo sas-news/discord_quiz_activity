688c6748c3ecc6b77c50bd506e7142d24d6735de
SAS NEWS <109570410+sas-news@users.noreply.github.com>
2024-09-06 19:01:10 +0900

    コード整理

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,54 +1,15 @@
 import { useEffect, useState } from "react";
-import { DiscordSDK, Types } from "@discord/embedded-app-sdk";
+import { Types } from "@discord/embedded-app-sdk";
+import discordSdk, { setupDiscordSdk } from "./Discord";
 
 const App = () => {
-  let auth;
-
-  const discordSdk = new DiscordSDK(import.meta.env.VITE_DISCORD_CLIENT_ID);
-
-  const setupDiscordSdk = async () => {
-    await discordSdk.ready();
-    console.log("Discord SDK is ready");
-
-    const { code } = await discordSdk.commands.authorize({
-      client_id: import.meta.env.VITE_DISCORD_CLIENT_ID,
-      response_type: "code",
-      state: "",
-      prompt: "none",
-      scope: ["identify", "guilds", "guilds.members.read"],
-    });
-
-    // Retrieve an access_token from your activity's server
-    const response = await fetch("/.proxy/api/token", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        code,
-      }),
-    });
-    const { access_token } = await response.json();
-
-    // Authenticate with Discord client (using the access_token)
-    auth = await discordSdk.commands.authenticate({
-      access_token,
-    });
-
-    if (auth == null) {
-      throw new Error("Authenticate command failed");
-    }
-
-    console.log("Authenticated with access token:", auth.access_token);
-  };
-
   const [voiceChannelName, setVoiceChannelName] = useState("");
   const [playerList, setPlayerList] = useState<
     Types.GetActivityInstanceConnectedParticipantsResponse["participants"]
   >([]);
 
   useEffect(() => {
-    setupDiscordSdk().then(() => {
+    setupDiscordSdk().then((auth) => {
       console.log("Discord SDK is authenticated");
       appendVoiceChannelName();
       appendPlayer();
@@ -58,10 +19,7 @@ const App = () => {
   const appendVoiceChannelName = async () => {
     let activityChannelName = "Unknown";
 
-    // Requesting the channel in GDMs (when the guild ID is null) requires
-    // the dm_channels.read scope which requires Discord approval.
     if (discordSdk.channelId != null && discordSdk.guildId != null) {
-      // Over RPC collect info about the channel
       const channel = await discordSdk.commands.getChannel({
         channel_id: discordSdk.channelId,
       });
---
+++ client/src/Discord.tsx
@@ -0,0 +1,47 @@
+import { DiscordSDK, patchUrlMappings } from "@discord/embedded-app-sdk";
+
+const discordSdk = new DiscordSDK(import.meta.env.VITE_DISCORD_CLIENT_ID);
+
+const setupDiscordSdk = async () => {
+  await discordSdk.ready();
+  console.log("Discord SDK is ready");
+
+  const { code } = await discordSdk.commands.authorize({
+    client_id: import.meta.env.VITE_DISCORD_CLIENT_ID,
+    response_type: "code",
+    state: "",
+    prompt: "none",
+    scope: ["identify", "guilds", "guilds.members.read"],
+  });
+
+  const response = await fetch("/.proxy/api/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      code,
+    }),
+  });
+  const { access_token } = await response.json();
+
+  const auth = await discordSdk.commands.authenticate({
+    access_token,
+  });
+
+  if (auth == null) {
+    throw new Error("Authenticate command failed");
+  }
+
+  console.log("Discord SDK is authenticated");
+
+  patchUrlMappings([
+    { prefix: "/api/firestore", target: "https://firestore.googleapis.com" },
+  ]);
+
+  return auth;
+};
+
+export { setupDiscordSdk };
+
+export default discordSdk;
